38d093e2ea1fe3afc1239dacab8f99d8504c9961
Matthew Wright <matthew.wright9630@gmail.com>
2025-12-02 09:12:10 -0500

    Add Spring JPA to Warehouse and Location.java

---
+++ inventory-management-backend/src/main/java/com/skillstorm/inventory_management_backend/Location.java
@@ -0,0 +1,128 @@
+package com.skillstorm.inventory_management_backend;
+
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="location")
+public class Location {
+    
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String country;
+
+    @Column(name = "state_or_region")
+    private String stateOrRegion;
+
+    @OneToMany
+    @JsonIgnore
+    private Set<Warehouse> warehouses;
+
+    public Location() {
+    }
+
+    public Location(int id, String country, String stateOrRegion, Set<Warehouse> warehouses) {
+        this.id = id;
+        this.country = country;
+        this.stateOrRegion = stateOrRegion;
+        this.warehouses = warehouses;
+    }
+
+    public Location(String country, String stateOrRegion, Set<Warehouse> warehouses) {
+        this.country = country;
+        this.stateOrRegion = stateOrRegion;
+        this.warehouses = warehouses;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getStateOrRegion() {
+        return stateOrRegion;
+    }
+
+    public void setStateOrRegion(String stateOrRegion) {
+        this.stateOrRegion = stateOrRegion;
+    }
+
+    
+    public Set<Warehouse> getWarehouses() {
+        return warehouses;
+    }
+
+    public void setWarehouses(Set<Warehouse> warehouses) {
+        this.warehouses = warehouses;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        result = prime * result + ((country == null) ? 0 : country.hashCode());
+        result = prime * result + ((stateOrRegion == null) ? 0 : stateOrRegion.hashCode());
+        result = prime * result + ((warehouses == null) ? 0 : warehouses.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Location other = (Location) obj;
+        if (id != other.id)
+            return false;
+        if (country == null) {
+            if (other.country != null)
+                return false;
+        } else if (!country.equals(other.country))
+            return false;
+        if (stateOrRegion == null) {
+            if (other.stateOrRegion != null)
+                return false;
+        } else if (!stateOrRegion.equals(other.stateOrRegion))
+            return false;
+        if (warehouses == null) {
+            if (other.warehouses != null)
+                return false;
+        } else if (!warehouses.equals(other.warehouses))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Location [id=" + id + ", country=" + country + ", stateOrRegion=" + stateOrRegion + "]";
+    }
+
+}
--- inventory-management-backend/src/main/java/com/skillstorm/inventory_management_backend/Warehouse.java
+++ inventory-management-backend/src/main/java/com/skillstorm/inventory_management_backend/Warehouse.java
@@ -1,15 +1,170 @@
 package com.skillstorm.inventory_management_backend;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
 public class Warehouse {
 
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @Column
     private String name;
 
+    @Column(name = "max_capacity")
     private int maximumCapacity;
 
+    @Column
+    private String address;
+    
+    @Column(name = "address_two")
+    private String addressLineTwo;
+
+    @ManyToOne
+    @JoinColumn(name= "location_id")
     private Location location;
     
+    public Warehouse() {
+    }
+
+    public Warehouse(int id, String name, int maximumCapacity, String address, String addressLineTwo, Location location) {
+        this.id = id;
+        this.name = name;
+        this.maximumCapacity = maximumCapacity;
+        this.address = address;
+        this.addressLineTwo = addressLineTwo;
+        this.location = location;
+        }
+
+    public Warehouse(String name, int maximumCapacity, String address, String addressLineTwo, Location location) {
+        this.name = name;
+        this.maximumCapacity = maximumCapacity;
+        this.address = address;
+        this.addressLineTwo = addressLineTwo;
+        this.location = location;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public int getMaximumCapacity() {
+        return maximumCapacity;
+    }
+
+
+    public void setMaximumCapacity(int maximumCapacity) {
+        this.maximumCapacity = maximumCapacity;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddressLineTwo() {
+        return addressLineTwo;
+    }
+
+    public void setAddressLineTwo(String addressLineTwo) {
+        this.addressLineTwo = addressLineTwo;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + maximumCapacity;
+        result = prime * result + ((address == null) ? 0 : address.hashCode());
+        result = prime * result + ((addressLineTwo == null) ? 0 : addressLineTwo.hashCode());
+        result = prime * result + ((location == null) ? 0 : location.hashCode());
+        return result;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Warehouse [id=" + id + ", name=" + name + ", maximumCapacity=" + maximumCapacity + ", address="
+                + address + ", addressLineTwo=" + addressLineTwo + ", location=" + location + "]";
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Warehouse other = (Warehouse) obj;
+        if (id != other.id)
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (maximumCapacity != other.maximumCapacity)
+            return false;
+        if (address == null) {
+            if (other.address != null)
+                return false;
+        } else if (!address.equals(other.address))
+            return false;
+        if (addressLineTwo == null) {
+            if (other.addressLineTwo != null)
+                return false;
+        } else if (!addressLineTwo.equals(other.addressLineTwo))
+            return false;
+        if (location == null) {
+            if (other.location != null)
+                return false;
+        } else if (!location.equals(other.location))
+            return false;
+        return true;
+    }
+
+
+
+
+
     public static boolean hasThreeCharacters(String input) {
             return input.length() >= 3;
     }
