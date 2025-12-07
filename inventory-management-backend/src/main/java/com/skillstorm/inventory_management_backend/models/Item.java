958e236cc84d77e2f2689b84a8fabb090a4b225a
Matthew Wright <matthew.wright9630@gmail.com>
2025-12-07 12:38:49 -0500

    Create ItemDetails.java

---
+++ inventory-management-backend/src/main/java/com/skillstorm/inventory_management_backend/models/Item.java
@@ -0,0 +1,5 @@
+package com.skillstorm.inventory_management_backend.models;
+
+public class Item {
+
+}
--- inventory-management-backend/src/main/java/com/skillstorm/inventory_management_backend/models/ItemDetails.java
+++ inventory-management-backend/src/main/java/com/skillstorm/inventory_management_backend/models/ItemDetails.java
@@ -1,10 +1,15 @@
 package com.skillstorm.inventory_management_backend.models;
 
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class ItemDetails {
@@ -22,4 +27,118 @@ public class ItemDetails {
 
     @Column
     private String description;
+
+    @OneToMany(mappedBy = "item_details")
+    @JsonIgnore
+    private Set<Item> items;
+
+    public ItemDetails() {
+    }
+
+    public ItemDetails(int id, String name, String sku, String description, Set<Item> items) {
+        this.id = id;
+        this.name = name;
+        this.sku = sku;
+        this.description = description;
+        this.items = items;
+    }
+
+    public ItemDetails(String name, String sku, String description, Set<Item> items) {
+        this.name = name;
+        this.sku = sku;
+        this.description = description;
+        this.items = items;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(Set<Item> items) {
+        this.items = items;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((sku == null) ? 0 : sku.hashCode());
+        result = prime * result + ((description == null) ? 0 : description.hashCode());
+        result = prime * result + ((items == null) ? 0 : items.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ItemDetails other = (ItemDetails) obj;
+        if (id != other.id)
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (sku == null) {
+            if (other.sku != null)
+                return false;
+        } else if (!sku.equals(other.sku))
+            return false;
+        if (description == null) {
+            if (other.description != null)
+                return false;
+        } else if (!description.equals(other.description))
+            return false;
+        if (items == null) {
+            if (other.items != null)
+                return false;
+        } else if (!items.equals(other.items))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ItemDetails [id=" + id + ", name=" + name + ", sku=" + sku + ", description=" + description + "]";
+    }
+
 }
